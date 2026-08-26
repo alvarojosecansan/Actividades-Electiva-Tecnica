8ed50ac598fbfa9995337d06fd992b1b3442af1f
Álvaro José Cantillo Sánchez <alvarojosecansan@gmail.com>
2026-08-25 23:50:46 -0500

    Add files via upload

---
+++ EjerciciosInternet/ejercicio1.py
@@ -0,0 +1,10 @@
+#Declara una variable con la temperatura en grados Celsius.
+# Calcula e imprime el equivalente en Fahrenheit usando la fórmula: $(C \times 9/5) + 32$.
+
+print("Declare una variable de grados Celsisus")
+
+GradoC = int(input("Ingrese un grado: "))
+
+Resultado = GradoC * 9/5 + 32
+
+print ("El resultado es: ", Resultado)
